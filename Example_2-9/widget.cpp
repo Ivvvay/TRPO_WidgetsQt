185a6ca9bada7a02b10766ed758c9e1f3d0cce2c
Ivan Lepeshkin <ivvvay@gmail.com>
2024-02-27 23:01:18 +0700

    wrote example 2-9

--- Example_2-9/widget.cpp
+++ Example_2-9/widget.cpp
@@ -1,8 +1,20 @@
 #include "widget.h"
+#include <QTextCodec>
+#include <QBoxLayout>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("Windows-1251"));    // Кодек для локализации на русском
+    this->setWindowTitle("Обработка событий");                                  // Заголовок окна
+    area = new Area(this);                                                      // Создание Area
+    btn = new QPushButton("Завершить",this );                                   // Создание Кнопки
+    QVBoxLayout *layout = new QVBoxLayout(this);                                // Вертикальная разметка
+
+    layout->addWidget(area);                                                    // Помещение на вертикальную разметку зону с фигурами и кнопку
+    layout->addWidget(btn);
+
+    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));                     // Подключение сигнала нажатия кнопки к слоту закрытия окна
 }
 
 Widget::~Widget()
--- Example_2-9/widget.h
+++ Example_2-9/widget.h
@@ -2,10 +2,16 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include "area.h"
+#include <QPushButton>
 
 class Widget : public QWidget
 {
     Q_OBJECT
+
+protected:
+    Area * area;                                    // Область рисунка
+    QPushButton * btn;                              // Кнопка выхода
 
 public:
     Widget(QWidget *parent = nullptr);
